a0517d9c5ce6939207e917086556c332875e3695
Brian Cheung <bmcheung11@gmail.com>
2016-12-19 17:40:27 -0800

    stuff

--- client/assets/controllers/gameController.js
+++ client/assets/controllers/gameController.js
@@ -1,19 +1,20 @@
 app.controller('gameController', ['$scope','$http', '$location', '$rootScope', '$route',
 function(sc, http, loc, rs, r) {
-    sc.chatbox = "";
-    if(!rs.user){
-        loc.url('/');
-        return;
-    }
-    sc.send_message = function(){
-        rs.socket.emit('chat_send', {user: rs.user, message: sc.message, roomId: rs.room});
-    }
-    rs.socket.on('update_chat', function(data){
-        sc.$apply(function(){
-            sc.chatbox += '\n' + data.user + ': ' + data.message;
-            sc.message = "";
-        })
-        //r.reload();
-    });
-
+  sc.chatbox = "";
+  if(!rs.user){
+      loc.url('/');
+      return;
+  }
+  sc.send_message = function(){
+      rs.socket.emit('chat_send', {user: rs.user, message: sc.message, roomId: rs.room});
+  }
+  rs.socket.on('update_chat', function(data){
+      sc.$apply(function(){
+          sc.chatbox += '\n' + data.user + ': ' + data.message;
+          sc.message = "";
+      })
+      //r.reload();
+  });
+  // var textarea = document.getElementById('textbox');
+  // textarea.scrollTop = textarea.scrollHeight;
 }]);
