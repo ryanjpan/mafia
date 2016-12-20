64321d022dfcfd45898f61f573b5b37b6b6bd835
Brian Cheung <bmcheung11@gmail.com>
2016-12-19 18:08:34 -0800

    gluiest scroll

--- client/assets/app.js
+++ client/assets/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('app', ['ngRoute']);
+var app = angular.module('app', ['ngRoute', 'luegg.directives']);
 app.config(function ($routeProvider) {
     $routeProvider
         .when('/',{
